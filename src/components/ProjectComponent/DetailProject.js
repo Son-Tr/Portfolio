47d9b55e35ff9e94e0cfe6fc5005cdcd3e77396c
Son-Tr <sontran.kh1502@gmail.com>
2024-11-02 16:36:54 +0700

    update detail project component

--- src/components/ProjectComponent/DetailProject.js
+++ src/components/ProjectComponent/DetailProject.js
@@ -1,11 +1,55 @@
 import React from 'react'
 
-const DetailProject = ({ item, clickGetBack }) => {
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import "swiper/css/effect-fade";
+
+
+const DetailProject = ({ item, clickGetBack }) => {
     return (
-        <div>
-            <h1>{item.name}</h1>
-            <button className='button' onClick={clickGetBack}>back to project</button>
+        <div className='detail-project'>
+            <div className="list-img">
+                <Swiper
+                    style={{
+                        '--swiper-pagination-color': 'hsl(23, 100%, 75%)',
+                    }}
+                    loop={true}
+                    spaceBetween={30}
+                    effect={"fade"}
+                    slidesPerView={1}
+                    autoplay={{
+                        delay: 3000,
+                        disableOnInteraction: true,
+                    }}
+
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Autoplay, Pagination]}
+                >
+                    {item.img.map((img, index) => (
+                        <SwiperSlide>
+                            <img src={img} alt={index} />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+                <div className="wrap-btn">
+                    <a href={item.linkWeb} className="button" target='_blank'>Demo</a>
+                    <a href={item.linkGithub} className="button" target='_blank'>Code</a>
+                </div>
+            </div>
+            <div className="content">
+                <h1>{item.name}</h1>
+                <button className='button' onClick={clickGetBack}>back to project</button>
+            </div>
         </div>
     )
 }
